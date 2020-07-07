3c732229f32e2cbf74de2455f0b77ad37d49a07a
Jorge Santos <jorge@petable.care>
2020-07-07 16:14:10 +0100

    ...

--- public/translations/push.php
+++
@@ -1,9 +0,0 @@
-<?php
-$remote = 'origin';
-$branch = 'master';
-
-exec('git --git-dir=.gitone add .');
-exec('git --git-dir=.gitone commit -m "new strings on the way"');
-exec('git --git-dir=.gitone push ' . $remote . ' ' . $branch . ' --tags --force');
-
-?>
